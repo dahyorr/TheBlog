cbc804c410e2a8a16b340b8d92c6f71029838191
Dayo Adebanjo <43008594+dah-yor@users.noreply.github.com>
2020-05-28 11:16:04 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,7 @@ class PostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['Title'] = self.model.title
+        context['Title'] = 'post'
         return context
 
 
